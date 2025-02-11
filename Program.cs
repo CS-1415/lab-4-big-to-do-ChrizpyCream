14090820af1806579474fd373efca9edb5c72ca6
Christopher Hercules <chriherc215@gmail.com>
2025-02-11 11:09:43 -0700

    1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿﻿class TodoListApp
+{
+    private TodoList _tasks;
+    private bool _showHelp = true;
+    private bool _insertMode = true;
+    private bool _quit = false;
+
+    public TodoListApp(TodoList tasks)
+    {
+        _tasks = tasks;
+    }
+
+    public void Run()
+    {
+        while (!_quit)
+        {
+            Console.Clear();
+            Display();
+            ProcessUserInput();
+        }
+    }
